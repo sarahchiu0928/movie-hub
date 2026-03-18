9a85c8172975478513402692eb23f379c48ef335
Sarah <x123719@gmail.com>
2026-03-18 10:28:39 +0800

    modify: 更換電影iocn

--- src/api/useGetmovieCredits.ts
+++ src/api/useGetmovieCredits.ts
@@ -3,30 +3,24 @@ import { computed, type Ref } from 'vue'
 import { axiosInstance } from '../utils/axiosInstance'
 import type { TmdbCredits } from '../types/movies'
 
-
-
-export const movieCreditsQueryKey = (id: string | number) => [
-'/movie',
-String(id),
-'credits'
-]
+export const movieCreditsQueryKey = (id: string | number) => ['/movie', String(id), 'credits']
 
 export const movieCreditsQueryFn = async (id: string): Promise<TmdbCredits> => {
   const res = await axiosInstance.get<TmdbCredits>(`/movie/${id}/credits`, {
-    params: { language: 'zh-TW' }
+    params: { language: 'zh-TW' },
   })
 
-return res.data
+  return res.data
 }
 
 export const useGetMovieCredits = (
-id: Ref<string>,
-options?: Omit<UseQueryOptions<TmdbCredits>, 'queryKey' | 'queryFn'>
+  id: Ref<string>,
+  options?: Omit<UseQueryOptions<TmdbCredits>, 'queryKey' | 'queryFn'>
 ) => {
-return useQuery<TmdbCredits>({
-queryKey: computed(() => movieCreditsQueryKey(id.value)),
-queryFn: () => movieCreditsQueryFn(id.value),
-enabled: computed(() => !!id.value),
-...options
-})
+  return useQuery<TmdbCredits>({
+    queryKey: computed(() => movieCreditsQueryKey(id.value)),
+    queryFn: () => movieCreditsQueryFn(id.value),
+    enabled: computed(() => !!id.value),
+    ...options,
+  })
 }
